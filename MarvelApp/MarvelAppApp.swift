ed0f1faae45be22819a599859b4bea0c2a1ab1f4
Marco Alonso Rodriguez <mrodriguez@epica.solutions>
2024-05-14 21:49:14 -0600

    Initial Commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Marco Alonso Rodriguez on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
